ff041be7bb515aa3c82368914615a5120e1f9993
GCorbel <guirec.corbel@gmail.com>
2014-02-02 23:23:22 +0000

    communicate with a socket server

--- turtle_web.py
+++ turtle_web.py
@@ -2,9 +2,15 @@ import cherrypy, os, sys, getopt
 from mako.template import Template
 from mako.lookup import TemplateLookup
 from optparse import OptionParser
+import socket
+import itertools
+from optparse import OptionParser
 
 lookup = TemplateLookup(directories=['public'])
-PUBLIC_DIR = os.path.join(os.path.abspath("."), u"public")
+PUBLIC_DIR = os.path.join(os.path.abspath("."), "public")
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect(("", 8081))
 
 class TurtleWeb(object):
     @cherrypy.expose
@@ -19,19 +25,23 @@ class TurtleWeb(object):
             rudder = None,
             aileron = None):
         if elevator:
-            print(self.percent_to_mu(elevator))
+            self.send('{"%s": "%s"}' %(23, self.percent_to_mu(elevator)))
         if throttle:
-            print(self.percent_to_mu(throttle))
+            self.send('{"%s": "%s"}' %(25, self.percent_to_mu(throttle)))
         if rudder:
-            print(self.percent_to_mu(rudder))
+            self.send('{"%s": "%s"}' %(4, self.percent_to_mu(rudder)))
         if aileron:
-            print(self.percent_to_mu(aileron))
+            self.send('{"%s": "%s"}' %(18, self.percent_to_mu(aileron)))
         cherrypy.response.headers['Content-Type'] = 'application/json'
         return b"ok"
     index.exposed = True
 
+    def send(self, json):
+        s.send(bytes(json.encode()))
+        str(s.recv(1000))
+
     def percent_to_mu(self, percent):
-        return 1090 + int(percent) * 810 / 100
+        return int(int(1090 + int(percent) * 810 / 100) / 10) * 10
 
 config = {'/public': {
     'tools.staticdir.on': True,
